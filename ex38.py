3b3db3be061bb796148ded55eca7a6fff31d495c
YunMeMeThaw <yunmemethaw16@gmail.com>
2018-06-24 08:41:32 -0700

    ex38.py

--- ex38.py
+++ ex38.py
@@ -1,17 +1,22 @@
-ten_things = "Apples Oranges Crows Telephone Light Sugar"
-print("Wait there are not 10 things in that list. Let's fix that.")
-stuff = ten_things.split(' ') 
-more_stuff = ["Day", "Night", "Song", "Frisbee", 7 "Corn", "Banana", "Girl", "Boy"]
-while len(stuff) != 10: 
-    next_one = more_stuff.pop() 
-    print("Adding: ", next_one) 
-    stuff.append(next_one) 
-    print(f"There are {len(stuff)} items now.")
-print("There we go: ", stuff)
-print("Let's do some things with stuff.")
-print(stuff[1]) 
-print(stuff[-1]) # whoa! fancy 
-print(stuff.pop()) 
-print(' '.join(stuff)) # what? cool! 
-print('#'.join(stuff[3:5])) # super stellar!
+ten_things = "Apples Oranges cows Telephone Light Sugar"
 
+print ("Wait there are not 10 things in that list. Let's fix")
+
+stuff = ten_things.split(' ')
+more_stuff =  {"Day", "Night", "Song", "Firebee",
+				"Corn", "Banana", "Girl", "Boy"}
+while len(stuff) !=10:
+	next_one = more_stuff.pop()
+	print("Adding: ", next_one)
+	stuff.append(next_one)
+	print (f"There are {len(stuff)} items n ow.")
+	
+print ("There we go : ", stuff)
+
+print ("Let's do some things with stuff.")
+
+print (stuff[1])
+print (stuff[-1]) # whoa! cool!
+print (stuff.pop())
+print (' '.join(stuff)) # what? cool !
+print ('#'.join(stuff[3:5])) #super stealler!
