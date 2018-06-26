7e1fa4359a91a786359280820d84bbd6736d7182
YunMeMeThaw <yunmemethaw16@gmail.com>
2018-06-25 19:36:25 -0700

    add ex101.py

---
+++ ex101.py
@@ -0,0 +1,16 @@
+tabby_cat = "\tI'm tabbed in"
+perrsian_cat = "I'm split\non a line."
+
+blackslash_cat = "I'm \\a \\cat."
+
+fat_cat = """
+I'll do a lst:
+\t* Cat food
+\t* Finishes
+\t Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(perrsian_cat)
+print(blackslash_cat)
+print(fat_cat)
